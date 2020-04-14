494c968c3e829d606a44d5a740276aa7a4a4f748
wangmaoxiong <2268461750@qq.com>
2020-04-14 08:26:51 +0800

    项目初始化

---
+++ src/main/java/com/wmx/cglibapp/CglibAppApplication.java
@@ -0,0 +1,13 @@
+package com.wmx.cglibapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CglibAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CglibAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmx/cglibapp/CglibAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmx.cglibapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CglibAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
