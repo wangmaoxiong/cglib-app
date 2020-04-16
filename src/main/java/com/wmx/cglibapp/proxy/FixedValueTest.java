910020f36a7a65b0331031c9dbe29774cc48dc94
wangmaoxiong <2268461750@qq.com>
2020-04-16 20:11:34 +0800

    FixedValue 回调对拦截方法统一返回固定值.

--- src/main/java/com/wmx/cglibapp/proxy/FixedValueTest.java
+++ src/main/java/com/wmx/cglibapp/proxy/FixedValueTest.java
@@ -42,6 +42,10 @@ public class FixedValueTest {
         //如果需要，生成一个新类并使用指定的回调（如果有）以创建新的对象实例。使用超类的无参数构造函数创建。
         FixedValueTest fixedValueTest = (FixedValueTest) enhancer.create();
         String toGreet = fixedValueTest.toGreet();
+        //输出：class com.wmx.cglibapp.proxy.FixedValueTest$$EnhancerByCGLIB$$24849247
+        //类名由 cglib 随机生成，以避免命名冲突
+        System.out.println(fixedValueTest.getClass());
+        //输出：{"code":200,"msg":"success"}
         System.out.println(toGreet);
     }
 }
