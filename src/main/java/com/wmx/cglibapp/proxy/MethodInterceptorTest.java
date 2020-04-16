d5a6d9bba59f9b88448e601cda24fb85500a5bde
Jessica <2268461750@qq.com>
2020-04-17 07:08:40 +0800

    代码优化.

--- src/main/java/com/wmx/cglibapp/proxy/MethodInterceptorTest.java
+++ src/main/java/com/wmx/cglibapp/proxy/MethodInterceptorTest.java
@@ -32,6 +32,8 @@ public class MethodInterceptorTest {
             //intercept 方法返回的数据类型，必须与拦截方法的实际返回类型兼容，否则类型转换异常.
             @Override
             public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                //第一次输出：[wmx]、第二次输出：[]
+                System.out.println(Arrays.asList(args));
                 // 如果调用的方法是继承的 Object 类的，则返回固定字符串，反则调用代理类.
                 if (method.getDeclaringClass() == Object.class) {
                     return "Hello Object !";
@@ -40,8 +42,6 @@ public class MethodInterceptorTest {
                      *  obj：增强对象，必须是作为 MethodInterceptor 的第一个参数传递的对象
                      *  args：传递给被拦截方法的参数；这里可以替换为不同的参数数组，只要类型兼容
                      */
-                    //输出：[wmx]
-                    System.out.println(Arrays.asList(args));
                     //替换传入的参数
                     args = new Object[]{"新化大熊山"};
                     return proxy.invokeSuper(obj, args);
