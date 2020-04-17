9ff9b509f9c600fa421675a730340059f7f2649b
wangmaoxiong <2268461750@qq.com>
2020-04-17 14:34:04 +0800

    ImmutableBean 不可变包装 bean.

---
+++ src/main/java/com/wmx/cglibapp/proxy/ImmutableBeanTest.java
@@ -0,0 +1,31 @@
+package com.wmx.cglibapp.proxy;
+
+import net.sf.cglib.beans.ImmutableBean;
+
+/**
+ * @author wangmaoxiong
+ * @version 1.0
+ * @date 2020/4/17 14:14
+ */
+public class ImmutableBeanTest {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setId(9527);
+        person.setName("华安");
+
+        //创建不可变包装 bean
+        Person immutablePerson = (Person) ImmutableBean.create(person);
+
+        //输出：com.wmx.cglibapp.proxy.Person@277050dc
+        System.out.println(person);
+        //输出：com.wmx.cglibapp.proxy.Person$$ImmutableBeanByCGLIB$$dee8248a@5c29bfd
+        System.out.println(immutablePerson);
+
+        //输出：9527,华安
+        System.out.println(immutablePerson.getId() + "," + immutablePerson.getName());
+        person.setName("虚竹");
+        //输出：9527,虚竹
+        System.out.println(immutablePerson.getId() + "," + immutablePerson.getName());
+        //如果操作 immutablePerson.setId(110);，则非法状态异常
+    }
+}
