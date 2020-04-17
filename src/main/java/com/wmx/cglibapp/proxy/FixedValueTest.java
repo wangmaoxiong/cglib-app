ac00adce29c7b6e690128eb617c69728e16746f7
wangmaoxiong <2268461750@qq.com>
2020-04-17 08:37:46 +0800

    代码优化.

--- src/main/java/com/wmx/cglibapp/proxy/FixedValueTest.java
+++ src/main/java/com/wmx/cglibapp/proxy/FixedValueTest.java
@@ -26,7 +26,7 @@ public class FixedValueTest {
         Enhancer enhancer = new Enhancer();
         //setSuperclass(Class superclass)：设置将扩展的类，superclass：要扩展的超类或要实现的接口
         enhancer.setSuperclass(FixedValueTest.class);
-        //interface FixedValue extends Callback：对拦截的所有方法返回固定的值
+        //interface FixedValue extends Callback：对拦截的所有方法返回固定的值，目标方法执行前，就会先被拦截.
         enhancer.setCallback(new FixedValue() {
             //返回的对象必须与的目标类拦截方法返回类型兼容，否则抛异常.
             @Override
@@ -41,6 +41,7 @@ public class FixedValueTest {
         });
         //如果需要，生成一个新类并使用指定的回调（如果有）以创建新的对象实例。使用超类的无参数构造函数创建。
         FixedValueTest fixedValueTest = (FixedValueTest) enhancer.create();
+        //目标方法调用时，就会被上面的 FixedValue 拦截
         String toGreet = fixedValueTest.toGreet();
         //输出：class com.wmx.cglibapp.proxy.FixedValueTest$$EnhancerByCGLIB$$24849247
         //类名由 cglib 随机生成，以避免命名冲突
