e9bae33f1da6223562ff0169e55bfd27d9456de8
wangmaoxiong <2268461750@qq.com>
2020-04-17 08:35:56 +0800

    代码优化.

--- src/main/java/com/wmx/cglibapp/proxy/HelloWorld.java
+++ src/main/java/com/wmx/cglibapp/proxy/HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
         /**setCallback(final Callback callback)
          * 设置要使用的单个 Callback，如果使用 {@link Enhancer#createClass()}，则会忽略，
          * callback 用于所有方法的回调
-         * {@link MethodInterceptor} 方法拦截接口，继承了 {@link Callback}，目标对象的所有方法调用都会被 intercept 方法拦截.
+         * {@link MethodInterceptor} 方法拦截接口，继承了 {@link Callback}，目标对象的所有方法调用时都会被 intercept 方法拦截.
          */
         enhancer.setCallback(new MethodInterceptor() {
             @Override
@@ -50,6 +50,7 @@ public class HelloWorld {
         //如果需要，生成一个新类并使用指定的回调（如果有）以创建新的对象实例。使用超类的无参数构造函数创建。
         HelloWorld helloWorld = (HelloWorld) enhancer.create();
         System.out.println("-------1----------");
+        //这里调用方法时，上面的 MethodInterceptor 就会拦截.
         helloWorld.toGreet();
         System.out.println("-------2---从 Object 继承的 toString 方法，可以被拦截-------");
         helloWorld.toString();
