9dae7b7bb566e9de80553660a44f30e5b06594f8
wangmaoxiong <2268461750@qq.com>
2020-04-18 21:45:10 +0800

    BeanMap 对象属性转 Map 对象.

--- src/main/java/com/wmx/cglibapp/beans/BeanMapTest.java
+++ src/main/java/com/wmx/cglibapp/beans/BeanMapTest.java
@@ -14,15 +14,15 @@ public class BeanMapTest {
         person.setId(2002);
         person.setName("刘邦");
 
-        BeanMap beanMap = BeanMap.create(person);
+        BeanMap personBeanMap = BeanMap.create(person);
         //输出：{birthday=null, marry=null, name=刘邦, id=2002, salary=null}
-        System.out.println(beanMap);
-        beanMap.put("id", 3003);
+        System.out.println(personBeanMap);
+        personBeanMap.put("id", 3003);
         //输出：Person{id=3003, name='刘邦', marry=null, salary=null, birthday=null}
         System.out.println(person);
         person.setMarry(false);
         //输出：{birthday=null, marry=false, name=刘邦, id=3003, salary=null}
-        System.out.println(beanMap);
+        System.out.println(personBeanMap);
     }
 
 }
