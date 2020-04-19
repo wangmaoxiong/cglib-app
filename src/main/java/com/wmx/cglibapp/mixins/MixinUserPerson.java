a1077844c4a37b49135254aee8a9fe977ce18fb5
wangmaoxiong <2268461750@qq.com>
2020-04-19 11:25:20 +0800

    Mixin 混合多个接口为一个接口.

--- src/main/java/com/wmx/cglibapp/mixins/MixinUserPerson.java
+++ src/main/java/com/wmx/cglibapp/mixins/MixinUserPerson.java
@@ -1,10 +1,11 @@
 package com.wmx.cglibapp.mixins;
 
 /**
+ * 混合接口继承需要进行合并的接口，注意父接口中的方法继承到本接口中不能冲突，如方法名称和参数完全一致.
+ *
  * @author wangmaoxiong
  * @version 1.0
  * @date 2020/4/19 10:52
  */
 public interface MixinUserPerson extends PersonService, UserService {
-    void show();
 }
